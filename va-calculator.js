f97e11d8c83a7af009432553fc9bb3aa3f9a5064
JohnathanBaldera <74841308+JohnathanBaldera@users.noreply.github.com>
2021-09-29 00:11:39 -0400

    Button Removal Feature Added
    TODO: Recalculate after the removal of the button -> Refactor possible?

--- va-calculator.js
+++ va-calculator.js
@@ -1,6 +1,6 @@
 enteredDisabilityPercentages = []
 const disabilityNums = document.getElementsByClassName("disability-num")
-const enteredNums = document.getElementsByClassName(".entered-ratings")
+
 
 // Calculating the disability when Percentage Buttons Clicked/Updating DOM 
 for (let btn of disabilityNums) {
@@ -17,22 +17,30 @@ for (let btn of disabilityNums) {
             document.querySelector(".entered-ratings").innerHTML += `<button class="entered-rating-btn">${disPer}% X</button>`
             let calNum = leftoverPercentage * (disPer * .01)
             calNum = Math.round(calNum)
-            console.log(calNum)
             leftoverPercentage -= calNum
             document.querySelector(".disability-leftover-amount").textContent = `${leftoverPercentage}`
             document.querySelector(".true-percentage-amount").textContent = `${100 - leftoverPercentage}`
         }
         document.querySelector(".current-rating").textContent = `${Math.round((100 - leftoverPercentage) * .10) * 10}%`
         
-           
+        //Removing a percentage
+        // TODO: Recalulate once button is removed
+        const enteredNums = document.getElementsByClassName("entered-rating-btn")
+        for (let btn of enteredNums) {
+            btn.addEventListener("click", () => {
+                let percentage = btn.textContent.match(/(\d+)/)[0]
+                enteredDisabilityPercentages.pop(percentage)
+                btn.remove()
+            })
+        }
     })
-}
+           
+};
+    
+
+    
 
 
 // TODO: Remove Button Entered
-for (let btn of enteredNums) {
-    btn.addEventListener("click", () => {
-        console.log("remove btn")
-    })
-}
+
 
