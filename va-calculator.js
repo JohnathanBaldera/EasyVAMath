352ba07b01049d8c3111cd1957589e962d2ff29d
JohnathanBaldera <74841308+JohnathanBaldera@users.noreply.github.com>
2021-10-10 08:21:35 -0400

    Refactoring of code
    Created separate function to calculate percentages.
    
    Started event delegation to remove entered disability buttons

--- va-calculator.js
+++ va-calculator.js
@@ -1,51 +1,67 @@
-enteredDisabilityPercentages = []
+const enteredDisabilityPercentages = []
 const disabilityNums = document.getElementsByClassName("disability-num")
+const enteredRatingDiv = document.getElementById("entered-ratings")
 
+// Calculate the adding of a disability
+const calculateDisability = () => {
+    let leftoverPercentage = 100
+    enteredRatingDiv.innerHTML = ""
+        for (let disPer of enteredDisabilityPercentages) {
+            enteredRatingDiv.innerHTML += `<button class="entered-rating-btn">${disPer}% X</button>`
+            let calNum = Math.round(leftoverPercentage * (disPer * .01))
+            leftoverPercentage -= calNum
+            document.querySelector(".disability-leftover-amount").textContent = `${leftoverPercentage}`
+            document.querySelector(".true-percentage-amount").textContent = `${100 - leftoverPercentage}`
+            document.querySelector(".current-rating").textContent = `${Math.round((100 - leftoverPercentage) * .10) * 10}%`
+        }
+}
 
 // Calculating the disability when Percentage Buttons Clicked/Updating DOM 
 for (let btn of disabilityNums) {
     btn.addEventListener("click", () => {
-        let leftoverPercentage = 100
         let num = btn.textContent.match(/(\d+)/)[0]
         enteredDisabilityPercentages.push(num)
 
         //Sorting array in reversed order
         enteredDisabilityPercentages.sort(function(a, b){return b-a})
+        calculateDisability()
+    })
+}    
 
-        document.querySelector(".entered-ratings").innerHTML = ""
-        for (let disPer of enteredDisabilityPercentages) {
-            document.querySelector(".entered-ratings").innerHTML += `<button class="entered-rating-btn">${disPer}% X</button>`
-            let calNum = leftoverPercentage * (disPer * .01)
-            calNum = Math.round(calNum)
-            leftoverPercentage -= calNum
-            document.querySelector(".disability-leftover-amount").textContent = `${leftoverPercentage}`
-            document.querySelector(".true-percentage-amount").textContent = `${100 - leftoverPercentage}`
+enteredRatingDiv.addEventListener("click", (e) => {
+        if (e.target && e.target.className == 'entered-rating-btn') {
+            let num = e.target.textContent.match(/(\d+)/)[0]
+            enteredDisabilityPercentages.splice(enteredDisabilityPercentages.indexOf(num), 1)
+            console.log(enteredDisabilityPercentages)
+            // enteredRatingDiv.innerHTML += `<button class="entered-rating-btn">${disPer}% X</button>`            
+            
         }
-        document.querySelector(".current-rating").textContent = `${Math.round((100 - leftoverPercentage) * .10) * 10}%`
+})
+        
         
         //Removing a percentage
         // TODO: Recalulate once button is removed
-        const enteredNums = document.getElementsByClassName("entered-rating-btn")
-        for (let btn of enteredNums) {
-            btn.addEventListener("click", () => {
-                let percentage = btn.textContent.match(/(\d+)/)[0]
-                enteredDisabilityPercentages.pop(percentage)
 
-                let num = btn.textContent.match(/(\d+)/)[0]
-                enteredDisabilityPercentages.pop(num)
-                btn.remove()
-                
+        // const enteredNums = document.getElementsByClassName("entered-rating-btn")
+        // for (let enteredBtn of enteredNums) {
+        //     enteredBtn.addEventListener("click", () => {
+        //         let percentage = enteredBtn.textContent.match(/(\d+)/)[0]
+        //         let idx = enteredDisabilityPercentages.indexOf(percentage)
+        //         enteredDisabilityPercentages.splice(idx, 1)
 
-            })
-        }
-    })
+        //         document.querySelector(".entered-ratings").innerHTML = ""
+        //         for (let disPer of enteredDisabilityPercentages) {
+        //             document.querySelector(".entered-ratings").innerHTML += `<button class="entered-rating-btn">${disPer}% X</button>`
+        //             let calNum = 100 * (disPer * .01)
+        //             calNum = Math.round(calNum)
+        //             leftoverPercentage = 100 - calNum
+        //             document.querySelector(".disability-leftover-amount").textContent = `${leftoverPercentage}`
+        //             document.querySelector(".true-percentage-amount").textContent = `${100 - leftoverPercentage}`
+        //         }
+        //         document.querySelector(".current-rating").textContent = `${Math.round((100 - leftoverPercentage) * .10) * 10}%`                
+
+        //     })
+        // }
+//     })
            
-};
-    
-
-    
-
-
-// TODO: Remove Button Entered
-
-
+// };
