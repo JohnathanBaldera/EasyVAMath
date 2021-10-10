33c0e56917f68186d52e9fd017150ca8af50d97d
JohnathanBaldera <74841308+JohnathanBaldera@users.noreply.github.com>
2021-10-10 14:37:33 -0400

    Start of PWA Process

--- va-calculator.js
+++ va-calculator.js
@@ -39,3 +39,14 @@ enteredRatingDiv.addEventListener("click", (e) => {
             calculateDisability()
         }
 })
+
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then(serviceWorker => {
+        console.log("Service Worker registered: ", serviceWorker);
+      })
+      .catch(error => {
+        console.error("Error registering the Service Worker: ", error);
+      });
+  }
