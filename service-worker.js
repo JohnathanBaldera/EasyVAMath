0853ecd7d082b2ed6cf4bf2278f275aad95c4ea9
JohnathanBaldera <74841308+JohnathanBaldera@users.noreply.github.com>
2021-10-13 22:25:25 -0400

    Formatting and PWA

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,20 @@
-STATIC_CACHE_FILES = ["./index.html", "./style.css", ".va-calculator.js"]
+import {registerRoute} from 'workbox-routing';
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js');
+import {StaleWhileRevalidate} from 'workbox-strategies';
 
-workbox.routing.registerRoute(
-  ({request}) => request.destination === STATIC_CACHE_FILES,
-  new workbox.strategies.CacheFirst()
+registerRoute(
+  ({request}) => request.destination === 'va-calculator' ||
+                  request.destination === 'style',
+  new StaleWhileRevalidate({
+    cacheName: 'static-resources',
+  })
 );
+
+// STATIC_CACHE_FILES = ["./index.html", "./style.css", ".va-calculator.js"]
+
+// importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js');
+
+// workbox.routing.registerRoute(
+//   ({request}) => request.destination === STATIC_CACHE_FILES,
+//   new workbox.strategies.CacheFirst()
+// );
